34121b8dffc172f4a08e04012c0a40375203f3fd
ngugilovesyou <amsamgitau@gmail.com>
2026-06-17 09:40:08 -0400

    updated files

--- server/config.py
+++ server/config.py
@@ -6,16 +6,16 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def init_app(app):
-    db.init_app(app)
-    migrate.init_app(app, db)
-
     db_url = os.getenv("DATABASE_URL")
 
+    
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
 
+   
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///requests.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+   
     db.init_app(app)
     migrate.init_app(app, db)
